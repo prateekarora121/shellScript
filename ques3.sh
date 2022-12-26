b43716b69774657bb813e08969c68b493545a595
prateekarora121 <pratikarora2@gmail.com>
2022-12-26 15:17:41 +0530

    change

--- ques3.sh
+++ ques3.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-for i in $( grep -A1 -i --no-group-separator 'Today\|Tomorrow'  agenda.txt ) 
+#Code will scan through the passed file and  if there are any matches for the current or next day will be shown in table format
+
+if [[ ${#} -eq 0 ]]
+then
+        echo "Use the script like this: ${0} -file_name"
+        exit 2
+fi
+
+for i in $( grep -A1 -i --no-group-separator 'Today\|Tomorrow'  ${1} ) 
      do	
 	if [[ $i == 'Today' || $i == 'Tomorrow' ]]  
 	then	
